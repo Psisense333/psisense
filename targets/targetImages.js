edb2bfd76aa6e29501a2dea469fafe481c1860d4
kirjastus.om@gmail.com <kirjastus.om@gmail.com>
2025-12-15 20:04:16 +0200

    Add local target image mapping

---
+++ targets/targetImages.js
@@ -0,0 +1,43 @@
+export const targetImages = {
+  // LEVEL 1
+  "35M87K1": require("./level1/35M87K1.jpg"),
+  "5R48K31": require("./level1/5R48K31.png"),
+  "7GR53F4": require("./level1/7GR53F4.png"),
+  "9T48L53": require("./level1/9T48L53.png"),
+  "F34T95K": require("./level1/F34T95K.png"),
+  "K58RL692": require("./level1/K58RL692.png"),
+  "L84KF36": require("./level1/L84KF36.png"),
+
+  // LEVEL 2
+  "5F93K82": require("./level2/5F93K82.png"),
+  "8F4L92R": require("./level2/8F4L92R.png"),
+  "F35T48R": require("./level2/F35T48R.jpeg"),
+  "F82G49L": require("./level2/F82G49L.png"),
+  "FK48L35": require("./level2/FK48L35.jpg"),
+  "L59R23K": require("./level2/L59R23K.png"),
+  "LF98R432": require("./level2/LF98R432.png"),
+  "R82L31K": require("./level2/R82L31K.png"),
+  "R923GM5": require("./level2/R923GM5.png"),
+
+  // LEVEL 3
+  "9R3L21F": require("./level3/9R3L21F.jpg"),
+  "D91K35M": require("./level3/D91K35M.png"),
+  "G4F28L5": require("./level3/G4F28L5.jpg"),
+  "K36R48F": require("./level3/K36R48F.jpg"),
+  "T54K32R": require("./level3/T54K32R.jpg"),
+
+  // LEVEL 4
+  "35FK48M": require("./level4/35FK48M.jpg"),
+  "F431K2R": require("./level4/F431K2R.jpg"),
+  "K53R16F": require("./level4/K53R16F.jpg"),
+  "M94R35K": require("./level4/M94R35K.jpg"),
+  "R53K4F1": require("./level4/R53K4F1.jpg"),
+  "T53K19L": require("./level4/T53K19L.png"),
+
+  // LEVEL 5
+  "6R3KF75": require("./level5/6R3KF75.jpg"),
+  "8F4K5LR": require("./level5/8F4K5LR.jpg"),
+  "F84L3R5": require("./level5/F84L3R5.jpg"),
+  "RF58M3T": require("./level5/RF58M3T.jpg"),
+  "RG84L93": require("./level5/RG84L93.jpg"),
+};
